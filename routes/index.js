58a831c8a55e2fc734da137656aa453cdc7fd177
coco.yu <359274291@qq.com>
2017-07-21 13:50:45 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(path.join(__dirname,'/views/header.html'), { title: 'Express' });
+  res.send('wonderful');
 });
 
 module.exports = router;
